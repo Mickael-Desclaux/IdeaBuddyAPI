874b20c1910c1b196e6fceecf6375a5089e0b64b
Mickael-Desclaux <mickaeldesclaux@gmail.com>
2026-02-28 11:10:09 +0100

    use faster model + test zod schema for response structure

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,23 +1,36 @@
 import { mistral, type MistralLanguageModelOptions } from '@ai-sdk/mistral';
-import { generateText } from 'ai';
+import { generateText, Output } from 'ai';
 import { NextFunction, Request, Response } from 'express';
+import z from 'zod';
 
-const model = mistral('mistral-large-latest');
+const model = mistral('mistral-small-latest');
 
 export async function testAIResponse(_req: Request, res: Response, next: NextFunction) {
   try {
     const response = await generateText({
       model,
+      output: Output.object({
+        schema: z.object({
+          recipe: z.object({
+            name: z.string(),
+            difficulty: z.string(),
+            ingredients: z.array(z.string()),
+            instructions: z.array(z.string()),
+            mealSuggestion: z.string(),
+          })
+        })
+      }),
       prompt: "Comment faire une tarte aux pommes?",
       providerOptions: {
         mistral: {
           safePrompt: true,
           parallelToolCalls: false,
+          strictJsonSchema: true,
         } satisfies MistralLanguageModelOptions,
       },
     });
 
-    return res.status(200).json(response);
+    return res.status(200).json(response.output);
   } catch (error) {
     next(error);
   }
